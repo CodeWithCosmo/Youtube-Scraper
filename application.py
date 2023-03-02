81eb1602270d0d44a2f9cf4795d1219f7876e938
Lalit Singh Bisht <lb5470272@gmail.com>
2023-03-02 15:33:21 +0530

    Update

--- application.py
+++ application.py
@@ -1,15 +1,14 @@
-# url ="https://www.youtube.com/@PW-Foundation/videos"
-import csv
 import time
 import pymongo
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from flask_cors import cross_origin
 from flask import Flask, render_template, request
-# from selenium.webdriver.edge.service import Service as EdgeService
-# from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-app = Flask(__name__)  
+application = Flask(__name__)  
+app = application
 
 @app.route('/', methods=['GET'])  
 @cross_origin()
@@ -38,7 +37,13 @@ def index():
                 video_link = "https://www.youtube.com" + str((soup.find_all("a", {"id": "video-title-link"}))[i].get("href"))
                 thumbnail_link = (soup.find_all("img", {"class": "yt-core-image--fill-parent-height"}))[i].get("src")[0:50]
                 mydict = {"Title": title, "Views": view, "Upload": upload,"Video Link": video_link, "Thumbnail Link": thumbnail_link}
-                scrape.append(mydict)            
+                scrape.append(mydict)           
+            ###################!Stage 2######################
+            # client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")  # Local Server
+            client = pymongo.MongoClient("mongodb+srv://lalit547:4W2lDPlTbwrbqkte@youtubescrape.shbwtmx.mongodb.net/?retryWrites=true&w=majority")
+            mydb= client.YoutubeScrape
+            mycollection = mydb.LastFiveVideos
+            mycollection.insert_many(scrape)
             time.sleep(5)
             driver.quit()            
             return render_template('results.html', scrape=scrape[0:5])

50c9af00f778fdaacfdb469b7e5e12e486cc01b9
Lalit Singh Bisht <lb5470272@gmail.com>
2024-11-09 14:13:04 +0530

    Update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 from flask import Flask,render_template,request
 from src.utils import scrape_records,write_mongo
 from src.exception import CustomException
+from waitress import serve
 
 app = Flask(__name__)  
 
@@ -14,8 +15,14 @@ def scrape():
     try:
         handle = request.form['content']
         scrapes = scrape_records(handle)
+        
         write_mongo(scrapes)
-        return render_template('output.html',context=scrapes)
+        
+        return render_template('output.html', context=scrapes)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        app.logger.error(f"Error occurred: {str(e)}", exc_info=True)
+        return render_template('error.html', error_message=str(e)), 500
+    
+if __name__ == '__main__':
+       serve(app, host='0.0.0.0', port=8080)
